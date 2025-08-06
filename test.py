e28423116026e42a7c79ccc26e131e8a33186007
neeraj9436 <neerajkr.yadav.ece19@itbhu.ac.in>
2025-08-06 22:33:21 +0530

    only two line in test in main

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-# for i in range(5):
-#     print("Hello, You are inside test.py")
-
 print("This is a test script to demonstrate git operations.")
 
 print("This is a test script to demonstrate git operations.")
