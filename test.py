e66ab4c9a73e22c76970d7059714e3f74dd8fab3
neeraj9436 <neerajkr.yadav.ece19@itbhu.ac.in>
2025-08-06 22:25:00 +0530

    remove hello world line

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-for i in range(5):
-    print("Hello, You are inside test.py")
+# for i in range(5):
+#     print("Hello, You are inside test.py")
 
 print("This is a test script to demonstrate git operations.")
 
