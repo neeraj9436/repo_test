dcaea302fbae7ecfb668b9448518d48013bc71bb
neeraj9436 <neerajkr.yadav.ece19@itbhu.ac.in>
2025-08-06 20:15:19 +0530

    Initial bubble sort in main

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello, You are inside main.py")
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Last i elements are already sorted
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap if the element found is greater than the next element
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Original array:", arr)
+    bubble_sort(arr)
+    print("Sorted array:", arr)
